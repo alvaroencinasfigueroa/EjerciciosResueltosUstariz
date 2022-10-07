922786fe3502575452db58fbc82bfc916ea024af
Alvaro Encinas Figueroa <alvaro19aef@gmail.com>
2022-10-07 11:39:20 -0400

    primer commit

---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ust u1 = new Ust();
+            int r = u1.Ej1_suma10Numeros();
+            string s = r.ToString();
+            MessageBox.Show("EL RESULTADO DEL EJERCICIO 1 ES: "+s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Ust u2 = new Ust();
+            int f  = Convert.ToInt32(Interaction.InputBox("INGRESAR UN NÚMERO PARA CONOCER SU FACTORIAL:"));
+            int r = u2.Ej2_factorialA(f);
+            string s = r.ToString();
+            MessageBox.Show("EL FACTORIAL ES: "+s);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Ust u3 = new Ust();
+            int resultadoPrimeros20Pares = u3.suma20Pares_b();
+            string pares20 = resultadoPrimeros20Pares.ToString();
+            MessageBox.Show("LA SUMA DE LOS PRIMEROS 20 NÚMEROS PARES ES: "+pares20);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Ust u4 = new Ust();
+            int resultado30MultiplosDe3 = u4.suma10MultiplosDe3_a();
+            string multiplos30 = resultado30MultiplosDe3.ToString();
+            MessageBox.Show("LA SUMA DE LOS PRIMEROS 10 MULTIPLOS DE 3 ES: "+multiplos30);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Ust u5 = new Ust();
+            int [] multiplos7 = u5.multiplosDe7B();    
+            for (int i = 1; i < multiplos7.Length; i++)
+            {
+                MessageBox.Show(""+multiplos7[i]);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Ust u6 = new Ust();
+            int ent = Convert.ToInt32(Interaction.InputBox("INGRESAR UN NÚMERO PARA CALCULAR SU CUADRADO: "));
+            int cuadrado = u6.calcularCuadrado(ent);
+            MessageBox.Show("EL CUADRADO DEL NÚMERO QUE USTED HA INGRESADO ES: "+cuadrado);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Ust u7 = new Ust();
+            double pi = 3.1416;
+            MessageBox.Show("EL PI ES: "+pi);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Ust u8 = new Ust();
+            double sumaSerie = u8.sumaSerie1();
+            MessageBox.Show("LA SUMA DE LA SERIE ES: "+sumaSerie);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Ust u9 = new Ust();
+            double sumaSerie2 = u9.sumaSerie2();
+            MessageBox.Show("LA SUMA DE LA SERIE ES: "+sumaSerie2);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        { 
+            Ust u10 = new Ust();
+            long[] serie = u10.mostrarSerieA_b();
+            for(int i = 0; i < serie.Length; i++)
+            {
+                MessageBox.Show(" "+serie[i]);
+            }
+        }
+
+    }
+}
---
+++ Ust.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Ust
+    {
+        //Ejercicio 1
+        public int Ej1_suma10Numeros()
+        {
+            int s = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                s = s + i;
+            }
+            return s;
+        }
+
+        //Ejercicio 2
+        public int Ej2_factorialA(int n)
+        {
+            int factorial = 1;
+            for (int c = 1; c <= n; c++)
+            {
+                factorial = factorial * c;
+            }
+            return factorial;
+        }
+
+        //Ejercicio 3
+        public int suma20Pares_b()
+        {
+            int s = 0;
+            for (int c = 2; c <= 40; c++)
+            {
+                if (c % 2 == 0)
+                {
+                    s = s + c;
+                }
+            }
+            return s;
+        }
+
+        //Ejercicio 4 a
+        public int suma10MultiplosDe3_a()
+        {
+            int s = 1, k = 0;
+            for (int c = 1; c < 30 || k == 10; c++)
+            {
+                if (c % 3 == 0)
+                {
+                    s = s + c;
+                    k++;
+                }
+            }
+            return s;
+        }
+
+        //Ejercicio 5
+        public int[] multiplosDe7B()
+        {
+            int ac = 105;
+            int[] res = new int[15];
+            res[1] = ac;
+            for (int c = 2; c <= 14; c++)
+            {
+                ac = ac + 7;
+                res[c] = ac;
+            }
+            return res;
+        }
+
+        //Ejercicio 6
+        public int calcularCuadrado(int num)
+        {
+            int s = 1;
+            int c = 1;
+            for (int k = 1; k < num; k++)
+            {
+                c = c + 2; // este hace el trabajo según el enunciado o sea 4 -> 1+3+5+7=16
+                s = s + c;
+            }
+            return s;
+        }
+
+        // Ejercicio 7
+        static int calcularPIGregory()
+        {
+            int n = -1;
+            int s = 0;
+            int d = 1;
+            int t = 0;
+            for (int c = 1; c < 100; c++)
+            {
+                n = n * (-1);
+                d = d * 2;
+                t = n / d;
+                s = s + t;
+            }
+            return s;
+        }
+
+        //Ejercicio 8
+        public double sumaSerie1()
+        {
+            double n;
+            double d = 0;
+            double s = 0;
+            double t;
+            for (int c = 1; c <= 10; c++)
+            {
+                n = 1;
+                d = d + 1;
+                t = n / d;
+                s = s + t;
+            }
+            return s;
+        }
+
+        //Ejercicio 9
+        public double sumaSerie2()
+        {
+            double n = -1;
+            double d = 1;
+            double s = 1;
+            double t = n / d;
+            s = s + t;
+            for (int c = 1; c <= 10; c++)
+            {
+                n = n + 2;
+                d = d * 2;
+                t = n / d;
+                s = s + t;
+            }
+            return s;
+        }
+
+        //Ejercicio 10
+        public long[] mostrarSerieA_b()
+        {
+            long ac = 1;
+            long[] res = new long[11];
+            res[0] = ac;
+            for (int c = 1; c <= 10; c++)
+            {
+                ac = ac * 3;
+                res[c] = ac;
+            }
+            return res;
+        }
+
+        public void mostrarVector(long [] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = 0;
+            }
+        }
+
+    }
+}
